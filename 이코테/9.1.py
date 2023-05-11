cb937305a52c2c5a426cc8097171347b16206ad5
Lee-Siyoung <tldud4861@gmail.com>
2023-05-11 23:22:00 +0900

    Create 9.1.py

---
+++ 이코테/9.1.py
@@ -0,0 +1,44 @@
+from sys import stdin
+
+input=int(stdin.readline())
+INF=int(1e9)
+
+n,m=map(int,stdin.readline().split())
+start=int(stdin.readline())
+graph=[[] for i in range(n+1)]
+visited=[False]*(n+1)
+distance=[INF]*(n+1)
+
+for _ in range(m):
+    a,b,c = map(int,stdin.readline().split())
+    graph[a].append((b,c))
+    
+def get_smallest_node():
+    min_value=INF
+    index=0
+    for i in range(1,n+1):
+        if distance[i] < min and not visited[i]:
+            min_value=distance[i]
+            index=i
+    return index
+    
+def dijkstra(start):
+    distance[start]=0
+    visited[start]=True
+    for j in graph[start]:
+        distance[j[0]]=j[1]
+    for i in range(n-1):
+        now=get_smallest_node()
+        visited[now]=True
+        for j in graph[now]:
+            cost=distance[now]+j[1]
+            if cost < distance[j[0]]:
+                distance[j[0]]=cost
+
+dijkstra(start)
+
+for i in range(1,n+1):
+    if distance[i]==INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
